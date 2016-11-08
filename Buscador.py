a93932e333ed4f4e1f459f3075c58770bd845048
tomas casadoumecq <tomas.casa123@gmail.com>
2016-11-07 22:53:40 -0300

    ejemplo

--- Buscador.py
+++ Buscador.py
@@ -1,15 +1,18 @@
-import telebot 
+import telebot
 
-TOKEN = '244028879:AAF8abnnbX_wwrjZpQMqCXqL8wOK6QrT44w' 
+TOKEN = '244028879:AAF8abnnbX_wwrjZpQMqCXqL8wOK6QrT44w' #Ponemos nuestro TOKEN generado con el @BotFather  
+mi_bot = telebot.TeleBot(TOKEN)                         #Creamos nuestra instancia "mi_bot" a partir de ese TOKEN
 
-bot = telebot.TeleBot(TOKEN)
+def listener(*mensajes):  ##Cuando llega un mensaje se ejecuta esta función  
+    for m in mensajes:
+        chat_id = m.chat.id
+        if m.content_type == 'text':
+            text = m.text
+            mi_bot.send_message(chat_id,"Me copio de tu texto")
+            mi_bot.send_message(chat_id, text)
 
-@bot.message_handler(commands=['start', 'help'])
-def command_help(message):
-    bot.reply_to(message, "Hola, escribe /buscar_cancion y el nombre de la cancion y te enviaremos un  link")
+mi_bot.set_update_listener(listener) #registrar la funcion listener  
+mi_bot.polling()
 
-
-bot.polling()
-
-
-
+while True: #No terminamos nuestro programa  
+    pass
