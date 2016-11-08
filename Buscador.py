061fd79adf5b956f2a541d84cd728aae34d35503
tomas casadoumecq <tomas.casa123@gmail.com>
2016-11-08 13:11:53 -0300

    prueba

--- Buscador.py
+++ Buscador.py
@@ -1,18 +1,15 @@
-import telebot
+import telebot 
 
-TOKEN = '244028879:AAF8abnnbX_wwrjZpQMqCXqL8wOK6QrT44w' #Ponemos nuestro TOKEN generado con el @BotFather  
-mi_bot = telebot.TeleBot(TOKEN)                         #Creamos nuestra instancia "mi_bot" a partir de ese TOKEN
+TOKEN = '244028879:AAF8abnnbX_wwrjZpQMqCXqL8wOK6QrT44w' 
 
-def listener(*mensajes):  ##Cuando llega un mensaje se ejecuta esta función  
-    for m in mensajes:
-        chat_id = m.chat.id
-        if m.content_type == 'text':
-            text = m.text
-            mi_bot.send_message(chat_id,"Me copio de tu texto")
-            mi_bot.send_message(chat_id, text)
+bot = telebot.TeleBot(TOKEN)
 
-mi_bot.set_update_listener(listener) #registrar la funcion listener  
-mi_bot.polling()
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hola, soy tu parlante SAIKO, que puedo hacer por vos?")
 
-while True: #No terminamos nuestro programa  
-    pass
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
