6015262bcb99ad2c56b88bca823390549956cfb6
tomas casadoumecq <tomas.casa123@gmail.com>
2016-11-07 22:45:59 -0300

    respuesta

--- Buscador.py
+++ Buscador.py
@@ -4,6 +4,12 @@ TOKEN = '244028879:AAF8abnnbX_wwrjZpQMqCXqL8wOK6QrT44w'
 
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(commands=['start', 'help'])
+def command_help(message):
+    bot.reply_to(message, "Hola, escribe /buscar_cancion y el nombre de la cancion y te enviaremos un  link")
 
-bot.send_message('244028879', 'hola mundo') # Ejemplo tb.send_message('109556849', 'Hola mundo!')
+
+bot.polling()
+
+
 
