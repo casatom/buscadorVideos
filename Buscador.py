4668e152a783310d7ef76d5b4e447e40914e54c1
tomas casadoumecq <tomas.casa123@gmail.com>
2016-11-08 23:16:17 -0200

    simple pero funciona

--- Buscador.py
+++ Buscador.py
@@ -1,11 +1,11 @@
 import telebot
 
-TOKEN = '244028879:AAF8abnnbX_wwrjZpQMqCXqL8wOK6QrT44w' #Ponemos nuestro TOKEN generado con el @BotFather  
+TOKEN = '234640505:AAH-CkZTORE4ItAurfEhvIZMLwBissgYsc8' #Ponemos nuestro TOKEN generado con el @BotFather  
 bot = telebot.TeleBot(TOKEN)                         #Creamos nuestra instancia "mi_bot" a partir de ese TOKEN
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hola, soy tu parlante SAIKO, que puedo hacer por vos?")
+    bot.reply_to(message, "Hola, queres buscar un video?")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
