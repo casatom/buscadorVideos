61b33efa1c4824c92767aa3f8cf92f26ce860414
casatom <tomas.casa123@gmail.com>
2016-11-08 21:41:10 -0300

    Update Buscador.py

--- Buscador.py
+++ Buscador.py
@@ -1,6 +1,4 @@
 import telebot
-from pyquery import PyQuery as Pq
-from urllib import urlencode
 
 TOKEN = '234640505:AAH-CkZTORE4ItAurfEhvIZMLwBissgYsc8' #Ponemos nuestro TOKEN generado con el @BotFather  
 bot = telebot.TeleBot(TOKEN)                         #Creamos nuestra instancia "mi_bot" a partir de ese TOKEN
@@ -8,8 +6,5 @@ bot = telebot.TeleBot(TOKEN)                         #Creamos nuestra instancia 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Hola, queres buscar un video?")
-
-
- 
-
+    
 bot.polling()
