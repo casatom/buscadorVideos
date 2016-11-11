57d6a0537c0b77b89d79c0dde5956dd735ccfbd2
tomasfedi <tvfedi@live.com.ar>
2016-11-11 15:03:32 -0300

    trabajo bien

--- Buscador.py
+++ Buscador.py
@@ -1,4 +1,8 @@
 import telebot
+import urllib.request
+import urllib.parse
+import six
+import re
 
 TOKEN = '234640505:AAH-CkZTORE4ItAurfEhvIZMLwBissgYsc8' #Ponemos nuestro TOKEN generado con el @BotFather  
 bot = telebot.TeleBot(TOKEN)                         #Creamos nuestra instancia "mi_bot" a partir de ese TOKEN
@@ -7,8 +11,13 @@ bot = telebot.TeleBot(TOKEN)                         #Creamos nuestra instancia 
 def send_welcome(message):
     bot.reply_to(message, "Hola, queres buscar un video?")
 
-@bot.message_handler(commands=['buscar_video'])
-def send_welcome(message):
-    bot.reply_to("Hola, que video queres buscar")
-
 bot.polling()
+
+@bot.message_handler(func=lambda message: True)
+def buscar_video(message):
+	print("entre")
+	query_string = urllib.parse.urlencode({"search_query" : message})
+	html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+	search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+	print("http://www.youtube.com/watch?v=" + search_results[0])	
+
